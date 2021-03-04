2fcfcaee2e4d891faf5ded9d57ee97d621f5395d
Nenad Savkic <savkicn@gmail.com>
2021-03-04 10:33:08 +0100

    Prvi commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.master')
+
+@section('title')
+    Welcome
+@endsection
+
+@section('main')
+
+<div class="container">
+    <h1 class="mt-5">Nenadov sajt</h1>
+
+    <article>
+        <h2>Moj CV</h2>
+        <div class="row">
+        <p class="col-6">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo earum dolorum odit ullam. Maiores deleniti asperiores consequuntur odio veritatis nihil officiis at libero corrupti eius laborum, expedita eos dolor quasi.
+        Cupiditate laborum ullam, quae laudantium iure cumque assumenda impedit perspiciatis nostrum et quasi fugiat, odio est. Sequi, doloribus delectus deleniti architecto recusandae voluptas minus. Eveniet et dicta iure ullam mollitia.
+        Quisquam eaque eveniet quidem a. Qui dolores ea aut voluptates magnam fugiat. At minus, explicabo distinctio asperiores error eius quisquam, aliquid eaque fugiat magni illum rerum, nam id iure delectus?
+        Error reiciendis ad repellat possimus a nulla dolores nihil deserunt cum illum magni eum neque doloremque similique omnis quis architecto, culpa unde consectetur eaque libero maiores qui beatae natus. Recusandae.
+        Eaque eligendi laudantium sapiente praesentium asperiores maiores quas quaerat, voluptatem quia illo recusandae, earum voluptate minima vitae repellendus, sit quidem voluptas. Veniam assumenda earum ex, necessitatibus maxime exercitationem impedit recusandae?
+        Ab debitis, provident, quo voluptates unde in labore ea placeat eum possimus enim deleniti eligendi eaque eius quas! Voluptates iusto earum accusantium vel nulla aliquam nesciunt maiores aliquid magnam similique.
+        Earum placeat quis maiores, quibusdam ea blanditiis adipisci tempora, alias temporibus aut quos quidem ut fuga ducimus debitis harum id suscipit nemo. Dolorum error ea, aperiam sunt delectus ratione quidem.
+        Exercitationem  enim ea sunt. Cumque!</p>
+
+        <img  src="/me/ja.jpg" alt="ja" class="col-6 img-fluid mb-5">
+        </div>
+    </article>
+</div>
+
+@endsection
