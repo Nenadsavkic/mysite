dfacc48fd38df3ef8796539987b2624d8033b19b
Nenad Savkic <savkicn@gmail.com>
2021-03-13 16:45:50 +0100

    Single ad prikaz zavrsen, ostale poruke i views

--- resources/views/singleAdCategory.blade.php
+++ resources/views/singleAdCategory.blade.php
@@ -19,6 +19,52 @@
 
         <div class="row mt-5 mb-5">
 
+            <div id="carouselSingleAd" class="col-md-12 carousel slide ml-1" data-ride="carousel">
+                <ol class="carousel-indicators">
+                  <li data-target="#carouselSingleAd" data-slide-to="0" class="active"></li>
+                  <li data-target="#carouselSingleAd" data-slide-to="1"></li>
+                  <li data-target="#carouselSingleAd" data-slide-to="2"></li>
+                  <li data-target="#carouselSingleAd" data-slide-to="3"></li>
+                  <li data-target="#carouselSingleAd" data-slide-to="4"></li>
+                </ol>
+                <div class="carousel-inner">
+                    @if (isset($ad->image1))
+                        <div class="carousel-item active">
+                            <img src="/images/add_images/{{ $ad->image1 }}" class="d-block w-100" alt="Image 1">
+                        </div>
+                    @endif
+                    @if (isset($ad->image2))
+                        <div class="carousel-item">
+                            <img src="/images/add_images/{{ $ad->image2 }}" class="d-block w-100" alt="Image 2">
+                        </div>
+                    @endif
+                    @if (isset($ad->image3))
+                        <div class="carousel-item">
+                            <img src="/images/add_images/{{ $ad->image3 }}" class="d-block w-100" alt="Image 3">
+                        </div>
+                    @endif
+                    @if (isset($ad->image4))
+                        <div class="carousel-item">
+                            <img src="/images/add_images/{{ $ad->image4 }}" class="d-block w-100" alt="Image 4">
+                        </div>
+                    @endif
+                    @if (isset($ad->image5))
+                        <div class="carousel-item">
+                            <img src="/images/add_images/{{ $ad->image5 }}" class="d-block w-100" alt="Image 5">
+                        </div>
+                    @endif
+
+                </div>
+                <a class="carousel-control-prev" href="#carouselSingleAd" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+
+                </a>
+                <a class="carousel-control-next" href="#carouselSingleAd" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+
+                </a>
+              </div>
+
 
                 @if (isset($ad->image1))
                 <div class="col-md-4 mt-5">
