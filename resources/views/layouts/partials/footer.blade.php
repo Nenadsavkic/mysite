9b4094e65874b0672dea9a6496854f1cc136d7a3
Nenad Savkic <58524230+Nenadsavkic@users.noreply.github.com>
2021-05-30 15:23:06 +0200

    Update footer.blade.php

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -9,7 +9,7 @@
 
               </div>
               <div class=" col-md-6 footer-content ">
-                <p class="text-center"><small>&#169;</small>Nenad Savkic 2021.</p>
+                <p class="text-center mt-1"><small>&#169;</small>Nenad Savkic 2021.</p>
              </div>
           </div>
     </div>
