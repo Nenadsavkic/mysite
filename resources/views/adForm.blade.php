29d6a95c4f1ecb1bbc86252d8236cc0f53716426
Nenad Savkic <58524230+Nenadsavkic@users.noreply.github.com>
2021-05-30 12:41:27 +0200

    Update adForm.blade.php

--- resources/views/adForm.blade.php
+++ resources/views/adForm.blade.php
@@ -14,7 +14,7 @@
 @section('content')
 <div class="container-fluid">
     <div class="row mt-5 mb-5">
-        <div class="col-md-2 sidebar">
+        <div class="col-md-3 offset-md-1 col-lg-2 sidebar">
             @include('layouts.partials.sidebar')
         </div>
 
